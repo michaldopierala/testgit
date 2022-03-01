9eef2bfa64c79cbdd24eba48b50de485ab9255cb
michal220222 <michal48778@gmail.com>
2022-03-01 07:02:45 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" type="image/png" href="favicon.png"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+</head>
+
+
+<title> MARKET</title>
+
+
+
+
+<body>
+  <div>
+    test git 
+  </div>
+</body>
+
+
+
+
+</html>
+
+
+
+
+
+
+
+
